5901fc9dcf37325b28a0d905a4776f349ac0df64
kayak <kayak@kayak.rocks>
2020-04-25 20:51:09 +1000

    feat(tutorial): step 5

--- script.js
+++ script.js
@@ -48,6 +48,40 @@ function create() {
   platforms.create(600, 400, "ground");
   platforms.create(50, 250, "ground");
   platforms.create(750, 220, "ground");
+
+  // Let's make a player!!!
+  // First, let's show the sprite. self explanatory, filename "dude", 100×450px
+  // from the bottom of the game.
+  player = this.physics.add.sprite(100, 450, "dude");
+
+  // bounce of 0.2 = landing after a jump will cause a slight bounce
+  player.setBounce(0.2);
+
+  // can't run off screen :)
+  player.setCollideWorldBounds(true);
+
+  // create running left animation called "left" (frames from filename "dude",
+  // runs @ 10fps, repeats forever)
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  // use frame 4 (0-indexed) to "turn", facing the camera as we do so
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
 }
 
 function update() {}
